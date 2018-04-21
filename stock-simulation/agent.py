9efa08cbfd4c87d19600473551c3ecd30066505b
zxchen <czxczf@gmail.com>
2018-04-21 14:53:14 +0200

    fulfill agent-d/agent-n test cases

--- stock-simulation/agent.py
+++ stock-simulation/agent.py
@@ -1,28 +1,30 @@
 import json
 import logging
-import unittest
 
-LOG = logging.getLogger(__name__).setLevel(logging.DEBUG)
+LOG = logging.getLogger(__name__)
+LOG.addHandler(logging.StreamHandler())
+LOG.setLevel(logging.DEBUG)
 
 STATE_WANT_TO_BUY = 0
 STATE_WANT_TO_SELL = 1
 
 
 class BaseAgent(object):
-    def __init__(self, state, connection=None, name="base"):
+
+    def __init__(self, state, name="Agent-Base"):
         self._state = state
         self.name = name
         self.price = None
         # connect to market and perform actions.
-        self.connection = connection
+        self.connection = None
 
     def buy(self, price):
         """Buy stock/bitcoin from market.
         price: the price of stock/bitcoin at timestamp t
         """
-        # if self.buying_signal(price):
         assert self.buying_signal(price), \
-            "{} cannot buy its stock/bitcoin.\n{}".format(self.name, self.__repr__())
+            "{} cannot buy a stock/bitcoin at price {}.\n{}".format(self.name, price,
+                                                                    self.__repr__())
 
         # TODO: buy this stock
         LOG.debug("{} buys a stock/bitconin at price {}.".format(self.name, price))
@@ -32,7 +34,9 @@ class BaseAgent(object):
 
         # Switch current state from `STATE_WANT_TO_BUY` to `STATE_WANT_TO_SELL`
         self._state = STATE_WANT_TO_SELL
-        # TODO: Emit a signal/message to a pool and waiting for others who want to buy.
+        # TODO: Emit a signal/message to a pool and wait for others who want to buy.
+        # Block until get the data
+        # self.connection.get()
 
     def sell(self, price):
         """Send stock/bitcoin to market.
@@ -40,6 +44,8 @@ class BaseAgent(object):
         """
         assert self.selling_signal(price), \
             "{} cannot sell its stock/bitcoin.\n{}".format(self.name, self.__repr__())
+        assert self.price, \
+            "{} must buy a stock/bitcoin before selling."
 
         # TODO: sell this stock
         LOG.debug("{} sells a stock/bitcoin at price {}, and gets profit {}."
@@ -48,13 +54,14 @@ class BaseAgent(object):
         self.price = None
         # Switch current state from `STATE_WANT_TO_SELL` to `STATE_WANT_TO_BUY`
         self._state = STATE_WANT_TO_BUY
-        # TODO: Emit a signal/message to a pool and waiting for others who want to sell
+        # TODO: Emit a signal/message to a pool and wait for others who want to sell
+        # self.connection.push()
 
     def buying_signal(self, price):
-        raise
+        raise NotImplementedError("buying_signal must be implemented in sub-class.")
 
     def selling_signal(self, price):
-        raise
+        raise NotImplementedError("selling_signal must be implemented in sub-class.")
 
     @property
     def state(self):
@@ -76,13 +83,13 @@ class AgentN(BaseAgent):
                  name="N-Agent"):
         # TODO: Set default value for `state`, `lower_bound` and `upper_bound`
         # from sound probability distributions
-        super(BaseAgent, self).__init__(state, name)
+        super(AgentN, self).__init__(state, name)
         self._lower_bound = lower_bound
         self._upper_bound = upper_bound
 
     def buying_signal(self, price):
         return self._state == STATE_WANT_TO_BUY and \
-            (price >= self._lower_bound)
+            (self._lower_bound >= price)
 
     def selling_signal(self, price):
         return self._state == STATE_WANT_TO_SELL and \
@@ -101,7 +108,7 @@ class AgentD(BaseAgent):
                  buyindg_threshold,
                  selling_threshold, name="D-Agent"):
         # `buyindg_threshold` and `selling_threshold` should be postive value
-        super(BaseAgent, self).__init__(state, name)
+        super(AgentD, self).__init__(state, name)
         self._buying_threshold = buyindg_threshold
         self._selling_threshold = selling_threshold
         self._tracked_min = None
@@ -109,43 +116,36 @@ class AgentD(BaseAgent):
 
     def tracking(self, price):
         if self._state == STATE_WANT_TO_BUY:
-            # sanity checking
-            if self._tracked_max is not None:
-                raise
-
+            assert self._tracked_max is None, "You're in state `STATE_WANT_TO_BUY`, `tracked_max` must be None."
             if self._tracked_min is None:
                 self._tracked_min = price
             else:
-                self._tracked_min = price if price < self._tracked_min else self._tracke_min
-
+                self._tracked_min = price if price < self._tracked_min else self._tracked_min
         elif self._state == STATE_WANT_TO_SELL:
-            # sanity checking
-            if self._tracked_min is not None:
-                raise
-
+            assert self._tracked_min is None, "You're in state `STATE_WANT_TO_SELL`, `tracked_min` must be None."
             if self._tracked_max is None:
                 self._tracked_max = price
             else:
-                self._tracked_max = price if price > self._tracked_max else self._tracke_max
+                self._tracked_max = price if price > self._tracked_max else self._tracked_max
         else:
             raise
 
-    def buying_singal(self, price):
+    def buying_signal(self, price):
         return (self._state == STATE_WANT_TO_BUY) and \
             (self._tracked_min is not None) and \
             (price - self._tracked_min) >= self._buying_threshold
 
     def selling_signal(self, price):
-        return (self._state == STATE_WAITING_TO_SELLING) and \
+        return (self._state == STATE_WANT_TO_SELL) and \
             (self._tracked_max is not None) and \
             (self._tracked_max - price) >= self._selling_threshold
 
     def buy(self, price):
-        super(BaseAgent, self).buy(price)
+        super(AgentD, self).buy(price)
         self._tracked_min = None
 
     def sell(self, price):
-        super(BaseAgent, self).sell(price)
+        super(AgentD, self).sell(price)
         self._tracked_max = None
 
     def __repr__(self):
@@ -156,81 +156,3 @@ class AgentD(BaseAgent):
                            "tracked_max": self._tracked_max,
                            "buyindg_threshold": self._buying_threshold,
                            "selling_threshold": self._selling_threshold})
-
-
-class AgentNTestCase(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_buying_signal(self):
-        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
-        self.assertTrue(agent_n.buying_signal(2))
-        self.assertTrue(agent_n.buying_signal(3))
-
-        self.assertFalse(agent_n.buying_signal(4))
-        self.assertFalse(agent_n.buying_signal(10))
-        self.assertFalse(agent_n.buying_signal(11))
-
-        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
-        self.assertFalse(agent_n.buying_signal(2))
-        self.assertFalse(agent_n.buying_signal(3))
-        self.assertFalse(agent_n.buying_signal(4))
-        self.assertFalse(agent_n.buying_signal(10))
-        self.assertFalse(agent_n.buying_signal(11))
-
-    def test_selling_signal(self):
-        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
-        self.assertTrue(agent_n.sellingsignal(11))
-        self.assertTrue(agent_n.sellingsignal(10))
-        self.assertFalse(agent_n.selling_signal(4))
-        self.assertFalse(agent_n.selling_signal(3))
-        self.assertFalse(agent_n.selling_signal(2))
-
-        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
-        self.assertFalse(agent_n.buying_signal(2))
-        self.assertFalse(agent_n.buying_signal(3))
-        self.assertFalse(agent_n.buying_signal(4))
-        self.assertFalse(agent_n.buying_signal(10))
-        self.assertFalse(agent_n.buying_signal(11))
-
-    def test_buy(self):
-        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
-        agent_n.buy(2)
-        self.assertEqual(agent_n.state, STATE_WANT_TO_SELL)
-        self.assertEqual(agent_n.price, 2)
-
-        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
-        agent_n.buy(3)
-        self.assertEqual(agent_n.state, STATE_WANT_TO_SELL)
-        self.assertEqual(agent_n.price, 3)
-
-        # TODO: The following code should raise exception, assert those exceptions
-        # agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
-        # agent_n.buy(1)
-
-        # agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
-        # agent_n.buy(3)
-
-    def test_sell(self):
-        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
-        agent_n.sell(11)
-        self.assertEqual(agent_n.state, STATE_WANT_TO_BUY)
-        self.assertIsNone(agent_n.price)
-
-        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
-        agent_n.sell(10)
-        self.assertEqual(agent_n.state, STATE_WANT_TO_BUY)
-        self.assertIsNone(agent_n.price)
-        # TODO: The following code should raise exceptions, assert those exceptions
-        # agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
-        # agent_n.sell(5)
-
-        # agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
-        # agent_n.sell(11)
-
-
-if __name__ == "__main__":
-    pass
---
+++ stock-simulation/tests.py
@@ -0,0 +1,234 @@
+import unittest
+from agent import AgentN, AgentD, STATE_WANT_TO_BUY, STATE_WANT_TO_SELL
+
+
+class AgentNTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_buying_signal(self):
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertTrue(agent_n.buying_signal(2))
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertTrue(agent_n.buying_signal(3))
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertFalse(agent_n.buying_signal(4))
+
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.buying_signal(2))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.buying_signal(3))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.buying_signal(4))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.buying_signal(10))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.buying_signal(11))
+
+    def test_selling_signal(self):
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertTrue(agent_n.selling_signal(11))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertTrue(agent_n.selling_signal(10))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.selling_signal(4))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.selling_signal(3))
+        agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+        self.assertFalse(agent_n.selling_signal(2))
+
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertFalse(agent_n.selling_signal(2))
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertFalse(agent_n.selling_signal(3))
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertFalse(agent_n.selling_signal(4))
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertFalse(agent_n.selling_signal(10))
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        self.assertFalse(agent_n.selling_signal(11))
+
+    def test_buy(self):
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        agent_n.buy(2)
+        self.assertEqual(agent_n.state, STATE_WANT_TO_SELL)
+        self.assertEqual(agent_n.price, 2)
+
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        agent_n.buy(3)
+        self.assertEqual(agent_n.state, STATE_WANT_TO_SELL)
+        self.assertEqual(agent_n.price, 3)
+
+        try:
+            agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+            agent_n.buy(5)
+        except AssertionError:
+            pass
+
+        try:
+            agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+            agent_n.buy(3)
+        except AssertionError:
+            pass
+
+    def test_sell(self):
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        agent_n.buy(2)
+        agent_n.sell(11)
+        self.assertEqual(agent_n.state, STATE_WANT_TO_BUY)
+        self.assertIsNone(agent_n.price)
+
+        agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+        agent_n.buy(2)
+        agent_n.sell(10)
+        self.assertEqual(agent_n.state, STATE_WANT_TO_BUY)
+        self.assertIsNone(agent_n.price)
+
+        try:
+            agent_n = AgentN(STATE_WANT_TO_BUY, 3, 10)
+            agent_n.sell(11)
+        except AssertionError:
+            pass
+
+        try:
+            agent_n = AgentN(STATE_WANT_TO_SELL, 3, 10)
+            agent_n.sell(10)
+        except AssertionError:
+            pass
+
+
+class AgentDTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_buying_signal(self):
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(2)
+        self.assertTrue(agent_d.buying_signal(7))
+
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(2)
+        self.assertFalse(agent_d.buying_signal(4))
+
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(3)
+        agent_d.tracking(1)
+        self.assertTrue(agent_d.buying_signal(5))
+
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(3)
+        agent_d.tracking(4)
+        self.assertTrue(agent_d.buying_signal(7))
+
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(3)
+        agent_d.tracking(1)
+        self.assertFalse(agent_d.buying_signal(3))
+
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(3)
+        agent_d.tracking(4)
+        self.assertFalse(agent_d.buying_signal(5))
+
+        agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+        agent_d.tracking(2)
+        self.assertFalse(agent_d.buying_signal(7))
+
+    def test_selling_signal(self):
+        agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+        agent_d.tracking(7)
+        self.assertTrue(agent_d.selling_signal(4))
+
+        agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+        agent_d.tracking(7)
+        self.assertFalse(agent_d.selling_signal(5))
+
+        agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+        agent_d.tracking(7)
+        agent_d.tracking(9)
+        self.assertTrue(agent_d.selling_signal(5))
+
+        agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+        agent_d.tracking(7)
+        agent_d.tracking(9)
+        self.assertFalse(agent_d.selling_signal(8))
+
+        agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+        agent_d.tracking(7)
+        agent_d.tracking(4)
+        self.assertTrue(agent_d.selling_signal(3))
+
+        agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+        agent_d.tracking(7)
+        agent_d.tracking(4)
+        self.assertFalse(agent_d.selling_signal(5))
+
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(7)
+        self.assertFalse(agent_d.selling_signal(2))
+
+    def test_buy(self):
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(3)
+        agent_d.tracking(2)
+        agent_d.tracking(5)
+        agent_d.buy(6)
+        self.assertEqual(agent_d.state, STATE_WANT_TO_SELL)
+        self.assertEqual(agent_d.price, 6)
+        self.assertIsNone(agent_d._tracked_min)
+
+        try:
+            agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+            agent_d.tracking(3)
+            agent_d.tracking(2)
+            agent_d.tracking(5)
+            agent_d.buy(4)
+        except AssertionError:
+            pass
+
+        try:
+            agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+            agent_d.tracking(3)
+            agent_d.tracking(2)
+            agent_d.tracking(5)
+            agent_d.buy(4)
+        except AssertionError:
+            pass
+
+    def test_sell(self):
+        agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+        agent_d.tracking(3)
+        agent_d.buy(7)
+        agent_d.tracking(8)
+        agent_d.tracking(9)
+        agent_d.sell(6)
+        self.assertEqual(agent_d.state, STATE_WANT_TO_BUY)
+        self.assertIsNone(agent_d.price)
+        self.assertIsNone(agent_d._tracked_max)
+
+        try:
+            agent_d = AgentD(STATE_WANT_TO_SELL, 4, 3)
+            agent_d.tracking(9)
+            agent_d.sell(6)
+        except AssertionError:
+            pass
+
+        try:
+            agent_d = AgentD(STATE_WANT_TO_BUY, 4, 3)
+            agent_d.tracking(3)
+            agent_d.buy(7)
+            agent_d.tracking(8)
+            agent_d.tracking(9)
+            agent_d.sell(8)
+        except AssertionError:
+            pass
+
+
+if __name__ == "__main__":
+    unittest.main()
