26c03a7a4294258dfa881135106bf70cbe0f92c6
zxchen <czxczf@gmail.com>
2018-04-26 07:56:46 +0200

    add log formatter

--- log.py
+++ log.py
@@ -1,8 +1,20 @@
 import logging
 
 
+def _get_formatter():
+    date_fmt = "%Y-%m-%d %H:%M:%S"
+    log_fmt = '%(asctime)s %(levelname)s %(name)s:%(lineno)d %(message)s'
+
+    formatter = logging.Formatter(log_fmt, date_fmt)
+    return formatter
+
+
 def getLogger(name):
+    formatter = _get_formatter()
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+
     logger = logging.getLogger(name)
-    logger.addHandler(logging.StreamHandler())
+    logger.addHandler(handler)
     logger.setLevel(logging.DEBUG)
     return logger
