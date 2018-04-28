1919cbe4b8e8495f061e74ecd2c8dec00c36f9f0
zxchen <czxczf@gmail.com>
2018-04-28 13:29:52 +0200

    successful for simulation

--- stock-simulation/main.py
+++ stock-simulation/main.py
@@ -20,7 +20,6 @@ class Simulation(object):
                  number_of_agentD=100,
                  mu=0,
                  sigma=2):
-        # seed = 1: counldnot produce sound result`
         reset_market()
         self.agentNs = factory.AgentFactory(agent.AgentN, number_of_agentN,
                                             agent_name="Agent-N")
@@ -35,10 +34,6 @@ class Simulation(object):
         LOG.info("Intializing...")
         LOG.debug("Factory {} has {} stocks".format(self.agentNs.name, self.agentNs.total_assets))
         LOG.debug("Factory {} has {} stocks".format(self.agentDs.name, self.agentDs.total_assets))
-        # for agentd in self.agentDs:
-        #     LOG.info(agentd)
-        # for agentn in self.agentNs:
-        #     LOG.info(agentn)
         LOG.info("Total stocks in the market is: {}, maximum stocks allowed in market is: {}.".format(
             self.agentNs.total_assets + self.agentDs.total_assets,
             self._number_of_agentN+self._number_of_agentD))
@@ -46,19 +41,21 @@ class Simulation(object):
         LOG.info("****************************************")
 
     def _simulate(self, noise, price, delta=0.01, max_iteration=10000):
-        LOG.info("Factory {} has {} stocks".format(self.agentNs.name, self.agentNs.total_assets))
-        LOG.info("Factory {} has {} stocks".format(self.agentDs.name, self.agentDs.total_assets))
+        LOG.debug("Factory {} has {} stocks".format(self.agentNs.name,
+                                                    self.agentNs.total_assets))
+        LOG.debug("Factory {} has {} stocks".format(self.agentDs.name,
+                                                    self.agentDs.total_assets))
         LOG.info("Noise is {}".format(noise))
         # number of external agents
         action = self._action(noise)
 
         if action == "buy" and self.agentNs.total_assets == 0:
             LOG.warn("No one wants to sell stocks/bitcoins in market.")
-            sys.exit(1)
+            return None
         if action == "sell" and \
            self.agentNs.total_assets == self.agentNs.length:
             LOG.warn("No one wants to buy stocks/bitcoins in market.")
-            sys.exit(1)
+            return None
 
         state = constants.STATE_WANT_TO_BUY \
             if action == "buy" else constants.STATE_WANT_TO_SELL
@@ -69,47 +66,52 @@ class Simulation(object):
         for agente in agentEs:
             getattr(agente, action)(price)
 
-        LOG.info("Number of buyer in market: {}".format(self.market.number_of_buyers))
-        LOG.info("Number of seller in market: {}".format(self.market.number_of_sellers))
-        # TODO: Here should be another check
+        LOG.debug("Number of buyer in market: {}".format(self.market.number_of_buyers))
+        LOG.debug("Number of seller in market: {}".format(self.market.number_of_sellers))
         delta = self._direction(action) * delta
         iteration = 0
-        # price += delta
         curr_diff, price = self._jump_to_next_unstable_price(price, action)
 
         while True:
+            iteration += 1
+            if iteration >= max_iteration:
+                LOG.error("Max iteration #{} reaches, price {} fail to find a solution for this transaction.".format(max_iteration, price))
+                return None
+
+            if iteration % 1000 == 0:
+                LOG.info("Iteration #{}: price {}.".format(iteration, price))
+                LOG.info("Previous difference is: {}, Current difference is: {}".format(prev_diff, curr_diff))
+                LOG.info("#{} agents want to buy stocks".format(
+                    self.market.number_of_buyers))
+                LOG.info("#{} agents want to sell stocks".format(
+                    self.market.number_of_sellers))
+
             price += delta
             prev_diff = curr_diff
             curr_diff = self._ballot(price)
 
-            if self.market.exchange(price):
-                LOG.info("Previous difference is: {}, Current difference is: {}".format(prev_diff, curr_diff))
-                LOG.info("Iteration #{}, reach stability price: {}".format(
-                    iteration, price))
-                break
-            if self._check_stable_price(prev_diff, curr_diff):
-                LOG.info("Previous difference is: {}, Current difference is: {}".format(prev_diff, curr_diff))
+            if self.market.exchange(price) or \
+               self._check_stable_price(prev_diff, curr_diff):
+                LOG.debug("Exchange stocks/bitcoins successfully.")
                 LOG.info("Iteration #{}, reach stability price: {}".format(
                     iteration, price))
                 break
 
-            iteration += 1
-            if iteration >= max_iteration:
-                LOG.error("Max iteration #{} reaches, price {} fail to find a solution for this transaction.".format(max_iteration, price))
-                sys.exit(1)
-            if iteration % 1000 == 0:
-                LOG.info("Iteration #{} --> price: {}.".format(iteration, price))
-                LOG.info("Previous difference is: {}, Current difference is: {}".format(prev_diff, curr_diff))
-        LOG.info("Factory {} has {} stocks".format(self.agentNs.name, self.agentNs.total_assets))
-        LOG.info("Factory {} has {} stocks".format(self.agentDs.name, self.agentDs.total_assets))
+        LOG.debug("Factory {} has {} stocks".format(self.agentNs.name,
+                                                    self.agentNs.total_assets))
+        LOG.debug("Factory {} has {} stocks".format(self.agentDs.name,
+                                                    self.agentDs.total_assets))
         LOG.info("Total assets in market is {}".format(self._total_assets))
         return price
 
-    def simulate(self, action=None, delta=0.00001, max_iteration=1000000):
+    def simulate(self, delta=0.001, max_iteration=50000):
         start = time.time()
+        LOG.info("****************************************")
         LOG.info("Start to simulate...")
+        LOG.info("****************************************")
         noise = self._generate_noise()
-        price = 10
+        _action = self._action(noise)
+        price = 0 if _action == "buy" else 1
         self.market.prices.append(price)
         for i in range(self._number_of_transactions):
             _price = price
@@ -117,8 +119,11 @@ class Simulation(object):
             _action = self._action(_noise)
             LOG.info("Round #{} ACTION: {}, PRICE: {}, DELTA: {}".format(
                 i, _action, _price, delta))
-            self._simulate(_noise, _price, delta, max_iteration)
-            price = self.market.prices[-1]
+            price = self._simulate(_noise, _price, delta, max_iteration)
+            # TODO: check #buyers and #sellers to decide which action should
+            # take place.
+            if price is None:
+                break
             noise = self._generate_noise()
 
         end = time.time()
@@ -131,8 +136,8 @@ class Simulation(object):
 
     def _generate_noise(self):
         """Generate external agents from gaussian distribution. This function ensures
-        the number of external agnets must be integral and non-zero
-        """
+        the number of external agnets must be integral and non-zero"""
+
         _noise = 0
         while _noise == 0:
             _noise = round(random.normalvariate(0, self._sigma))
@@ -151,19 +156,30 @@ class Simulation(object):
 
     @property
     def _total_assets(self):
+        """Total assets(stocks/bitcoins) in market."""
         return self.agentDs.total_assets + self.agentNs.total_assets
 
     def _direction(self, action):
         """If action is `buy` the price should go up, the direction of trend is
         positive. And action `sell` the price should go down, the direction of
-        trend is negative."""
+        trend is negative.
+        --------
+        Parameters:
+        action: only could be `buy` or `sell`
+        Returns: 1 if action is `buy`, 0 if action is `sell`
+        """
         return 1 if action == "buy" else -1
 
     def _ballot(self, price):
         """All agents in market ballot for given price. The `diff` between
         the number of seller and the number of buyer is what we are interested.
         Since it indicates how many stocks/bitcoins are bought/sold from/to
-        the markets."""
+        the markets.
+        --------
+        Parameter:
+        price: the price of stocks/bitcoins
+        Returns: difference of #buyers and #sellers, it's integral.
+        """
 
         for agentn in self.agentNs:
             try:
@@ -182,13 +198,9 @@ class Simulation(object):
                     agentd.sell(price)
             except AssertionError:
                 pass
-        LOG.debug("#{} agents want to buy stocks".format(
-            self.market.number_of_buyers))
-        LOG.debug("#{} agents want to sell stocks".format(
-            self.market.number_of_sellers))
         diff = self.market.number_of_buyers - self.market.number_of_sellers
 
-        return diff
+        return int(round(diff))
 
     def _jump_to_next_unstable_price(self, price, action, delta=0.001):
         diff = 0
@@ -207,23 +219,14 @@ class Simulation(object):
 
 
 if __name__ == "__main__":
-    number_of_transactions = 5
+    number_of_transactions = 100
     LOG.debug("****************************************")
-    LOG.debug("**Start to simlualte *buy* #{} transaction".format(number_of_transactions))
+    LOG.debug("**Start to simlualte #{} transaction".format(number_of_transactions))
     LOG.debug("****************************************")
-    random.seed(123)
+    random.seed(1)
     sim = Simulation(number_of_transactions,
-                     number_of_agentN=50,
-                     number_of_agentD=50)
-    sim.simulate("buy", 0.0001, 1000000)
+                     number_of_agentN=100,
+                     number_of_agentD=25)
+    sim.simulate(0.001, 100000)
     LOG.info("After #{} simulations, we got {}".format(number_of_transactions,
                                                        sim.market.prices))
-
-    # LOG.debug("****************************************")
-    # LOG.debug("**Start to simlualte *sell* #{} transaction".format(number_of_transactions))
-    # LOG.debug("****************************************")
-    # random.seed(1)
-    # sim = Simulation(number_of_transactions)
-    # sim.simulate("sell", 0.00001, 100000)
-    # LOG.info("After #{} simulations, we got {}".format(number_of_transactions,
-    #                                                    sim.market.prices))
